45fe15fc1fb05466f08e76dfad249a482db80f72
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-07 11:20:51 +0000

    Initial commit: reads in data from .csv file

---
+++ SupportBank/SupportBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = GetTransactions();
+            List<string[]> transactions = new List<string[]>();
+
+            foreach (string line in data)
+            {
+                string[] lineArray = line.Split(",");
+                transactions.Add(lineArray);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string[] GetTransactions()
+        {
+            return System.IO.File.ReadAllLines(@"C:\Users\JHG\Work\Training\SupportBank\Transactions2014.csv");
+        }
+    }
+}
