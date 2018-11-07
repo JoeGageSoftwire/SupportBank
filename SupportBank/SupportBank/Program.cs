2d9b1810bff6a395b8e3aa62d6b0a507ed84f56a
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-07 12:47:42 +0000

    Run through transaction data and calculate each person's credit or debit

--- SupportBank/SupportBank/Program.cs
+++ SupportBank/SupportBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SupportBank
 {
@@ -14,6 +15,29 @@ namespace SupportBank
             {
                 string[] lineArray = line.Split(",");
                 transactions.Add(lineArray);
+            }
+
+            transactions.RemoveAt(0);                           //Removes column headings
+
+            List<string> names = new List<string>();
+            foreach (string[] entry in transactions)
+            {
+                string giver = entry[1];
+                string receiver = entry[2];
+                names.Add(giver);
+                names.Add(receiver);
+            }
+            
+            List<string> namesUnique = names.Distinct().ToList();
+            List<double> balances = new List<double>(new double[namesUnique.Count]);
+
+            foreach (string[] entry in transactions)
+            {
+                int giverIndex = namesUnique.IndexOf(entry[1]);
+                int receiverIndex = namesUnique.IndexOf(entry[2]);
+                double transactionAmount = double.Parse(entry[4]);
+                balances[giverIndex] = balances[giverIndex] - transactionAmount;
+                balances[receiverIndex] = balances[receiverIndex] + transactionAmount;
             }
 
             Console.ReadLine();
